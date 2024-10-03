836b3d54eeedab7654d18cd95061ed9f38b59015
mosesfuraha <moses.furaha@amalitechtraining.org>
2024-10-03 20:02:37 +0200

    update the create component form

--- src/app/pages/blogs/blogs.component.ts
+++ src/app/pages/blogs/blogs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { BlogService } from '../../core/services/blog.service';
 import { Blog } from '../../models/common.model';
 
@@ -7,10 +7,11 @@ import { Blog } from '../../models/common.model';
   templateUrl: './blogs.component.html',
   styleUrls: ['./blogs.component.css'],
 })
-export class BlogsComponent implements OnInit {
+export class BlogsComponent {
   blogs: Blog[] = [];
   showSingleBlog: boolean = false;
   selectedBlog: Blog | null = null;
+  showOptions: boolean = false; // To track if dropdown is open
 
   constructor(private blogService: BlogService) {}
 
@@ -33,5 +34,40 @@ export class BlogsComponent implements OnInit {
   backToBlogs(): void {
     this.showSingleBlog = false;
     this.selectedBlog = null;
+  }
+
+  toggleOptions(blog: Blog): void {
+    if (this.selectedBlog === blog && this.showOptions) {
+      this.showOptions = false;
+    } else {
+      this.selectedBlog = blog;
+      this.showOptions = true;
+    }
+  }
+
+  editBlog(blog: Blog): void {
+    console.log('Editing blog:', blog);
+
+    this.showOptions = false;
+  }
+
+  deleteBlog(blog: Blog): void {
+    console.log('Deleting blog:', blog);
+
+    if (blog.id) {
+      this.blogService
+        .deleteBlog(blog.id)
+        .then(() => {
+          console.log('Blog deleted successfully');
+
+          this.blogs = this.blogs.filter((b) => b.id !== blog.id);
+          this.showOptions = false;
+        })
+        .catch((error) => {
+          console.error('Error deleting blog:', error);
+        });
+    } else {
+      console.error('Blog ID not found, cannot delete blog');
+    }
   }
 }
--- src/app/pages/create-blog/create-blog.component.ts
+++ src/app/pages/create-blog/create-blog.component.ts
@@ -2,6 +2,8 @@ import { Component, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BlogService } from '../../core/services/blog.service';
 import { Blog } from '../../models/common.model';
+import { AngularFireStorage } from '@angular/fire/compat/storage'; // Import Firebase Storage
+import { finalize } from 'rxjs/operators'; // Import to handle completion of upload process
 
 @Component({
   selector: 'app-create-blog',
@@ -10,10 +12,16 @@ import { Blog } from '../../models/common.model';
 })
 export class CreateBlogComponent {
   @Output() closeModal: EventEmitter<void> = new EventEmitter();
-
   blogForm: FormGroup;
+  selectedFile: File | null = null;
+  uploadProgress: number = 0;
+  imageUrl: string = ''; // Store the uploaded image URL
 
-  constructor(private fb: FormBuilder, private blogService: BlogService) {
+  constructor(
+    private fb: FormBuilder,
+    private blogService: BlogService,
+    private storage: AngularFireStorage // Inject AngularFireStorage
+  ) {
     this.blogForm = this.fb.group({
       title: ['', Validators.required],
       imageUrl: ['', Validators.required],
@@ -28,6 +36,44 @@ export class CreateBlogComponent {
   close(): void {
     this.closeModal.emit();
   }
+
+  // // Handle file selection
+  // onFileSelected(event: any): void {
+  //   const file: File = event.target.files[0];
+  //   if (file) {
+  //     this.selectedFile = file;
+  //     console.log('Selected file:', file);
+  //     this.uploadFile(file);
+  //   }
+  // }
+
+  // // Upload the selected file to Firebase Storage
+  // uploadFile(file: File): void {
+  //   const filePath = `blogs/${Date.now()}_${file.name}`; // Path to store the file
+  //   const fileRef = this.storage.ref(filePath); // Reference to the file
+  //   const task = this.storage.upload(filePath, file); // Upload task
+
+  //   // Monitor upload progress
+  //   task.percentageChanges().subscribe((progress) => {
+  //     if (progress) {
+  //       this.uploadProgress = progress;
+  //       console.log('Upload progress:', progress);
+  //     }
+  //   });
+
+  //   // Get download URL when upload completes
+  //   task
+  //     .snapshotChanges()
+  //     .pipe(
+  //       finalize(() => {
+  //         fileRef.getDownloadURL().subscribe((url) => {
+  //           console.log('File uploaded! Download URL:', url);
+  //           this.blogForm.patchValue({ imageUrl: url }); // Set the image URL in the form
+  //         });
+  //       })
+  //     )
+  //     .subscribe();
+  // }
 
   submit(): void {
     if (this.blogForm.invalid) {
